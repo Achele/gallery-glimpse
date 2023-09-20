9cdfbf7499d0eaee09aaaa9197147a213b4b6349
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-09-20 03:25:20 +0100

    set up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Gallery from "./pages/Gallery";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/signUp" element={<SignUp />} />
+      <Route path="/gallery" element={<Gallery />} />
+    </Routes>
+  );
 }
 
 export default App;

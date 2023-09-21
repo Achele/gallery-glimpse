4a39d21dcff91e35fd9829649cd5f7308bb25f2a
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-09-22 00:32:25 +0100

    update drag and drop functionality

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,9 +1,13 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Loading from "../components/common/Loading";
 import GalleryCard from "../components/GalleryCard";
 import SearchBar from "../components/SearchBar";
 import { UseUserAuth } from "../context/authContext";
 import useFetch from "../hooks/useFetch";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
+import { useState, useEffect } from "react";
 
 const Gallery = () => {
   const url = `https://api.pexels.com/v1/curated?per_page=12`;
@@ -11,6 +15,43 @@ const Gallery = () => {
   const { loading, data, error } = useFetch(url);
   const { user, logout } = UseUserAuth();
   const navigate = useNavigate();
+  const [images, setImages] = useState([]);
+  const [dragItemIndex, setDragItemIndex] = useState();
+  const [dragOverItemIndex, setDragOverItemIndex] = useState();
+
+  const handleDragStart = (index) => {
+    setDragItemIndex(index);
+  };
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  };
+
+  const handleDrop = (draggedIndex, targetIndex, draggedImage) => {
+    const _images = [...images];
+    const dragItem = _images.splice(draggedIndex, 1)[0];
+    _images.splice(targetIndex, 0, dragItem);
+    setImages(_images);
+  };
+
+  const handleDragEnter = (index) => {
+    setDragOverItemIndex(index);
+  };
+
+  const handleDragLeave = (event) => {
+    setDragOverItemIndex(undefined);
+  };
+
+  const handleDragEnd = (event) => {
+    setDragItemIndex(undefined);
+    setDragOverItemIndex(undefined);
+  };
+
+  useEffect(() => {
+    if (data) {
+      setImages(data);
+    }
+  }, [data]);
 
   const handleLogout = async () => {
     try {
@@ -21,7 +62,7 @@ const Gallery = () => {
       console.log(error.message);
     }
   };
-  console.log(data);
+  console.log(images);
   return (
     <main>
       <div className="flex items-center overflow-hidden relative w-full bg-indigo-100 py-12 px-8 my-0 mx-auto">
@@ -34,16 +75,31 @@ const Gallery = () => {
         </button>
       </div>
       <section className="w-4/5 my-0 mx-auto py-8 px-20">
-        {/* <h1>Pexels Gallery</h1> */}
-        {loading && <Loading />}
-        {error && <p>Error: {error.message}</p>}
-        {!loading && !error && (
-          <section className="grid md:grid-cols-2 xl:grid-cols-4 gap-12">
-            {data.map((images) => {
-              return <GalleryCard key={images.id} {...images} />;
-            })}
-          </section>
-        )}
+        <DndProvider backend={HTML5Backend}>
+          {loading && <Loading />}
+          {error && <p>Error: {error.message}</p>}
+          {!loading && !error && (
+            <section className="grid md:grid-cols-2 xl:grid-cols-4 gap-12">
+              {images.map((image, index) => {
+                return (
+                  <GalleryCard
+                    key={image.id}
+                    image={image}
+                    index={index}
+                    dragItemIndex={dragItemIndex}
+                    dragOverItemIndex={dragOverItemIndex}
+                    handleDragStart={handleDragStart}
+                    handleDragOver={handleDragOver}
+                    handleDrop={handleDrop}
+                    handleDragEnter={handleDragEnter}
+                    handleDragLeave={handleDragLeave}
+                    handleDragEnd={handleDragEnd}
+                  />
+                );
+              })}
+            </section>
+          )}
+        </DndProvider>
       </section>
     </main>
   );

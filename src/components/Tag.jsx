4d17690e89164eafd2a77f304aa1596ca92926fa
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-09-22 03:06:26 +0100

    update styling

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -2,7 +2,7 @@ const Tag = ({ tagText, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="bg-blue-500 text-white p-2 rounded-lg cursor-pointer mr-2"
+      className="bg-blue-500 text-white p-2 rounded-lg cursor-pointer mr-2 xs:text-xs xs:p-1 xs:m-0"
     >
       {tagText}
     </button>
